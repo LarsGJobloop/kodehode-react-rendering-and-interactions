56dba5acc864f76109c23108b658f4d798c559c8
larsgjobloop <lars.gunnar@jobloop.no>
2024-04-23 09:10:33 +0000

    inital setup

---
+++ rendering/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Layout} from './components/layout/layout'
+
+function App() {
+  return (
+    <Layout>
+      <section>
+          <h2 className='text-center'>Rendering Lists</h2>
+
+        </section>
+    </Layout>
+  )
+}
+
+export default App
---
+++ rendering/src/components/card/card.jsx
@@ -0,0 +1,9 @@
+import style from './card.module.css'
+
+export function Card({children}) {
+  return (
+    <div className={style["card"]}>
+      {children}
+    </div>
+  )
+}
